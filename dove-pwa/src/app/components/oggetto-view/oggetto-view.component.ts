4bcc72afc7064df1bb94a089077cccdf5c8848da
Daniele <d.tarini@hyperborea.com>
2021-11-14 12:23:13 +0100

    Update node and angular

--- dove-pwa/src/app/components/oggetto-view/oggetto-view.component.ts
+++ dove-pwa/src/app/components/oggetto-view/oggetto-view.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class OggettoViewComponent implements OnInit {
 
-  uuid: string;
+  uuid: string | null;
 
   constructor(
     private route: ActivatedRoute
