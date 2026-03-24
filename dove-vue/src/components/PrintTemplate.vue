43b288f87aeb9f0090db61bfbfd85c8c2cb37bfb
Daniele Tarini <daniele.tarini@horsa.it>
2026-03-24 17:46:47 +0100

    Full print, removed Vite dev

--- dove-vue/src/components/PrintTemplate.vue
+++ dove-vue/src/components/PrintTemplate.vue
@@ -101,103 +101,108 @@ function printCodes() {
 
 </script>
 <template>
-  <div class="fullscreen">
+  <div>
     <div class="controls noprint">
       <div>
-        <span>Righe {{ rows }}</span>
-        <button @click="rows = rows+1">+</button>
-        <button @click="rows = rows-1">-</button>
-      </div>
-      <div>
-        <span>Colonne {{ columns }}</span>
-        <button @click="columns++">+</button>
+        <span>Righe</span>
+        <button @click="rows--">-</button>
+        <input class="trbl" type="number" v-model="rows" size="2"/>
+        <button @click="rows++">+</button>
+        <span>Colonne</span>
         <button @click="columns--">-</button>
-      </div>
-      <div>
-        <button @click="printCodes()">Stampa</button>
+        <input class="trbl" type="number" v-model="columns" size="2"/>
+        <button @click="columns++">+</button>
       </div>
       <div>
         <span>
+          <span>Gap righe</span>
           <button @click="gapRows--">-</button>
           <input class="trbl" type="number" v-model="gapRows" size="3"/>
           <button @click="gapRows++">+</button>
         </span>
         <span>
+          <span>Gap colonne</span>
           <button @click="gapColumns--">-</button>
           <input class="trbl" type="number" v-model="gapColumns" size="3"/>
           <button @click="gapColumns++">+</button>
         </span>
       </div>
-    </div>
-    <div class="control-area" style="position: relative;">
-      <span class="control tlbr tlbr-top noprint">
+      <div>
         <span>
+          <span>top</span>
           <button @click="padding.top--">-</button>
           <input class="trbl" type="number" v-model="padding.top" size="3"/>
           <button @click="padding.top++">+</button>
         </span>
-      </span>
-      <span class="control tlbr tlbr-bottom noprint">
         <span>
+          <span>bottom</span>
           <button @click="padding.bottom--">-</button>
           <input class="trbl" type="number" v-model="padding.bottom" size="3"/>
           <button @click="padding.bottom++">+</button>
         </span>
-      </span>
-      <span class="control tlbr tlbr-left noprint">
+      </div>
+      <div>
         <span>
+          <span>left</span>
           <button @click="padding.left--">-</button>
           <input class="trbl" type="number" v-model="padding.left" size="3"/>
           <button @click="padding.left++">+</button>
         </span>
-      </span>
-      <span class="control tlbr tlbr-right noprint">
         <span>
+          <span>right</span>
           <button @click="padding.right--">-</button>
           <input class="trbl" type="number" v-model="padding.right" size="3"/>
           <button @click="padding.right++">+</button>
         </span>
-      </span>
-      <div class="preview-area">
-        <svg viewBox="0 0 210 297" preserveAspectRatio="xMidYMid meet">
-          <g v-for="(row, ridx) in codes">
-            <g v-for="(code, cidx) in row">
-              <rect fill="yellow" class="noprint"
-                v-bind="{
-                  x: (padding.left + cidx * (codeWidth() + gapColumns)), 
-                  y: (padding.top + ridx * (codeHeight() + gapRows)),
-                  width: codeWidth(),
-                  height: codeHeight(),
-                  }"
-              ></rect>
-              <foreignObject
-                v-bind="{
-                  x: (padding.left + cidx * (codeWidth() + gapColumns)) + (codeWidth() - codeSize()) / 2, 
-                  y: (padding.top + ridx * (codeHeight() + gapRows)) + (codeHeight() - codeSize()) / 2,
-                  width: codeSize(),
-                  height: codeSize(),
-                  }"
-              >
-                <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
-                  <rect x="0" y="0" width="100" height="100" fill="white"></rect>
-                  <!-- QR -->
-                  <foreignObject x="0" y="0" width="48" height="48">
-                    <qrcode-vue :value="code" :render-as="'svg'" :size="48" />
-                  </foreignObject>
-                  <foreignObject x="52" y="0" width="48" height="48" v-html="makeIdenticon(code)" />
-                  <!-- Testo -->
-                  <text v-for="(part, index) in textrows(code)"
-                    x="50" 
-                    v-bind:y="70 + index*14" 
-                    text-anchor="middle" 
-                    v-bind:font-size="13 + (index > 0? 0: 10)"
-                  >{{ part }}</text>
-                </svg>
-              </foreignObject>
-            </g>
-          </g>
-        </svg>
       </div>
+      <div>
+        <button @click="printCodes()">Stampa</button>
+      </div>
+    </div>
+    <div class="printfullpage">
+      <svg viewBox="0 0 210 297" preserveAspectRatio="xMidYMid meet" class="fullpage">
+        <rect x="0" y="0" width="210" height="297" fill="orange" class="noprint"></rect>
+        <rect x="0" y="0" width="210" :height="padding.top || 0" fill="brown" class="noprint"></rect>
+        <rect :y="297 - padding.bottom || 0" x="0" width="210" :height="padding.bottom || 0" fill="brown" class="noprint"></rect>
+        <rect x="0" y="0" :width="padding.left || 0" height="297" fill="brown" class="noprint"></rect>
+        <rect y="0" :x="210 - padding.right || 0" :width="padding.right || 0" height="297" fill="brown" class="noprint"></rect>
+        <g v-for="(row, ridx) in codes">
+          <g v-for="(code, cidx) in row">
+            <rect fill="yellow" class="noprint"
+              v-bind="{
+                x: (padding.left + cidx * (codeWidth() + gapColumns)), 
+                y: (padding.top + ridx * (codeHeight() + gapRows)),
+                width: codeWidth(),
+                height: codeHeight(),
+                }"
+            ></rect>
+            <foreignObject
+              v-bind="{
+                x: (padding.left + cidx * (codeWidth() + gapColumns)) + (codeWidth() - codeSize()) / 2, 
+                y: (padding.top + ridx * (codeHeight() + gapRows)) + (codeHeight() - codeSize()) / 2,
+                width: codeSize(),
+                height: codeSize(),
+                }"
+            >
+              <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+                <rect x="0" y="0" width="100" height="100" fill="white"></rect>
+                <!-- QR -->
+                <foreignObject x="0" y="0" width="48" height="48">
+                  <qrcode-vue :value="code" :render-as="'svg'" :size="48" />
+                </foreignObject>
+                <foreignObject x="52" y="0" width="48" height="48" v-html="makeIdenticon(code)" />
+                <!-- Testo -->
+                <text v-for="(part, index) in textrows(code)"
+                  x="50" 
+                  v-bind:y="70 + index*14" 
+                  text-anchor="middle" 
+                  v-bind:font-size="13 + (index > 0? 0: 10)"
+                >{{ part }}</text>
+              </svg>
+            </foreignObject>
+          </g>
+        </g>
+      </svg>
     </div>
   </div>
 </template>
@@ -228,42 +233,24 @@ input.trbl{
   top: 50%;
 }
 
-.fullscreen {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.preview-area {
-  width: 100%;
-  display: flex;
-  justify-content: center; /* centro orizzontale */
-  flex-direction: column;
-  background: lightgray; /* solo per vedere meglio */
-}
-
-.page-a4 {
-  /* overflow: hidden; */
-  background: white;
-  border: 1px solid black;
-  box-sizing: border-box;
-}
-
 /* PRINTABLE */
 
 @media print {
 
-  .page-a4 {
-    left: 0;
-    top: 0;
-    margin: 0;
-    width: 100vw;
-    position: fixed;
-    /* border: 1px solid gray; */
+  svg.fullpage {
+    display: block;
   }
 
-  .controls, .noprint, .header {
+  .printfullpage {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    /* height: 100vh; */
+    /* overflow: auto; */
+  }
+
+  .controls, .noprint {
     display: none;
   }
 
